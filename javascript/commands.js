3abecc4042ba6d8a3d8337cdb73d7e507bebbdb0
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-01-05 13:25:12 +0100

    added functions to install commands

---
+++ javascript/commands.js
@@ -0,0 +1,48 @@
+---
+---
+
+function get_null_command() {
+  return "#!/bin/bash\necho 'Nothing to do.'\n"
+}
+
+function command_not_found(name) {
+  return "#!/bin/bash\n1>&2 echo 'ERROR: the command \"" + name + "\" was not found.'\nexit 1";
+}
+
+
+var commands = {};
+
+function load_command(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var file_name = url.slice(url.lastIndexOf("/") + 1, url.length);
+      commands[file_name] = this.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+} 
+
+var static_files = {{ site.static_files | map: "path" | jsonify }};
+
+function load_commands() {
+  for (var i = 0; i < static_files.length; i += 1) {
+    var absolute_path = static_files[i];
+    if (absolute_path.startsWith("/commands")) {
+      var url = "." + absolute_path;
+      load_command(url);
+    }
+  }
+}
+
+window.onload = load_commands;
+
+function get_command_from_file_name(file_name) {
+  if (file_name in commands) {
+    return commands[file_name];
+  } else {
+    alert("ERROR: could not find command named \"" + file_name + "\". Please check the spelling.");
+    return command_not_found(file_name);
+  }
+};
--- javascript/install.js
+++ javascript/install.js
@@ -1,1 +1,42 @@
 
+function get_redirect_url() {
+  var path = document.location.pathname;
+  var end_of_directory = path.lastIndexOf("/");
+  var directory_path = path.slice(0, end_of_directory + 1);
+  return document.location.origin + directory_path + "installing.html";
+}
+
+function build_specification() {
+  // see https://github.com/CodersOS/image-creator-server/blob/master/docs/README.md
+  var spec = {};
+  spec.redirect = get_redirect_url();
+  spec.commands = [];
+  var installs = software_spec_to_install();
+  for (var i; i < installs.length; i += 1) {
+    var software_spec = installs[i];
+    var build_spec = get_build_command(software_spec);
+    spec.commands.push(build_spec);
+  }
+  return spec;
+}
+
+function get_build_command(software) {
+  var command = {};
+  command.name = software.id;
+  if (spec.script) {
+    command.command = get_command_from_file_name(spec.script);
+    if (spec.command) {
+      alert("You can not specify command and script in " + software.id + ".");
+    }
+  } else if (spec.command) {
+    command.command = spec.command;
+  } else {
+    command = get_null_command();
+  }
+  command.arguments = []; // TODO: Read parameters from options
+  return command;
+}
+
+function install_everything() {
+  
+}
