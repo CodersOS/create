9cb2c31511318666bd84d73bbd3c95f0d80f3fb3
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-01-12 10:37:17 +0100

    fix #11

--- javascript/commands.js
+++ javascript/commands.js
@@ -17,12 +17,51 @@ function load_command(url) {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var file_name = url.slice(url.lastIndexOf("/") + 1, url.length);
-      commands[file_name] = this.responseText;
+      set_command(file_name, this.responseText);
     }
   };
   xhttp.open("GET", url, true);
   xhttp.send();
 } 
+
+function set_command(command_name, command_code) {
+  if (command_code.slice(0, 2) == "#!") {
+    // this is a text command as it starts with "#!"
+    command_code = restore_linux_line_endings(command_code);
+  }
+  commands[command_name] = command_code;  
+}
+
+function restore_linux_line_endings(text) {
+  var system = detect_line_endings(text);
+  if (system == "windows") {
+    return text.replace(/\r\n/g, "\n");
+  } else if (system == "mac") {
+    return text.replace(/\r/g, "\n");
+  } else {
+    return text; // linux
+  }
+}
+
+function detect_line_endings(text) {
+  var detected = "none";
+  for ( var i = 0; i < text.length; i += 1 ) {
+    if (text[i] == "\r") {
+      if (detected == "none") {
+        detected = "mac";
+      }
+    } else if (text[i] == "\n") {
+      if (detected == "none") {
+        detected = "linux";
+      } else if (detected == "mac"){
+        detected = "windows";
+      }
+    } else if (detected != "none") {
+      break;
+    }
+  }
+  return detected;
+}
 
 var static_files = {{ site.static_files | map: "path" | jsonify }};
 
